f6038b06e57d8f25e84b487d9e69dac9e04408c2
Lily Havok <lokujou01@gmail.com>
2026-03-12 21:03:00 -0500

    Updated game.nobasic

--- NoBASIC/game.asm
+++ NoBASIC/game.asm
@@ -40,12 +40,15 @@ XOR R1, R1
 MOV P0, 298
 MOV P1, R1
 MOV [P0], P1
+MOV P1, STR38
+MOV P0, 300
+MOV [P0], P1
 MOV VM, 0
 MOV VL, 1
 XOR VX, VX
 XOR VY, VY
 MOV VC, 31
-TEXT STR28
+TEXT STR39
 MOV R1, 1
 SHL R1, 3
 MOV P2, R1
@@ -54,187 +57,76 @@ MOV R0, R1
 MOV R1, 1
 SHL R1, 3
 MOV P3, R1
-L30:
+L41:
 CMP P2, R0
-JGT L31
+JGT L42
 XOR VX, VX
 MOV VY, P2
 MOV VC, 31
-TEXT STR31
+TEXT STR42
 ADD P2, P3
-JMP L30
-L31:
+JMP L41
+L42:
 XOR VX, VX
 MOV VY, 248
 MOV VC, 31
-TEXT STR28
-MOV P1, STR32
-MOV P0, 300
-MOV [P0], P1
+TEXT STR39
 MOV VM, 0
 MOV VL, 5
-L34:
+L44:
 MOV R1, 1
 WHILE R1
-JZ L35
-Player:
-; Load Player.x
-MOV P0, 288
-MOV P1, [P0]
-; Store to Player.oldx
-MOV P0, 292
-MOV [P0], P1
-; Load Player.y
-MOV P0, 290
-MOV P1, [P0]
-; Store to Player.oldy
-MOV P0, 294
-MOV [P0], P1
-; Load Player.counter
-MOV P0, 298
-MOV P1, [P0]
-MOV R0, P1
-ADD P1, 1
-; Store to Player.counter
-MOV P0, 298
-MOV [P0], P1
+JZ L45
 PUSH P2
-CALL _func_keycheck_2
+CALL _func_callplayer_4
 POP P2
-; Load Player.x
-MOV P0, 288
-MOV P1, [P0]
-MOV R0, 1
-SHL R0, 3
-CMP P1, R0
-; Free P1 (last use)
-; Free R0 (last use)
-JGE L36
-MOV R1, 1
-SHL R1, 3
-; Store to Player.x
-MOV P0, 288
-MOV P1, R1
-MOV [P0], P1
-L36:
-; Load Player.x
-MOV P0, 288
-MOV P1, [P0]
-MOV R0, 240
-CMP P1, R0
-; Free P1 (last use)
-; Free R0 (last use)
-JLE L38
-MOV R1, 240
-; Store to Player.x
-MOV P0, 288
-MOV P1, R1
-MOV [P0], P1
-L38:
-; Load Player.y
-MOV P0, 290
-MOV P1, [P0]
-MOV R0, 1
-SHL R0, 3
-CMP P1, R0
-; Free P1 (last use)
-; Free R0 (last use)
-JGE L40
-MOV R1, 1
-SHL R1, 3
-; Store to Player.y
-MOV P0, 290
-MOV P1, R1
-MOV [P0], P1
-L40:
-; Load Player.y
-MOV P0, 290
-MOV P1, [P0]
-MOV R0, 232
-CMP P1, R0
-; Free P1 (last use)
-; Free R0 (last use)
-JLE L42
-MOV R1, 232
-; Store to Player.y
-MOV P0, 290
-MOV P1, R1
-MOV [P0], P1
-L42:
-PUSH P2
-CALL _func_drawplayer_0
-POP P2
-; Load Player.counter
-MOV P0, 298
-MOV P1, [P0]
-MOV R0, 255
-CMP P1, R0
-; Free P1 (last use)
-; Free R0 (last use)
-JNZ L44
-XOR R1, R1
-; Store to Player.counter
-MOV P0, 298
-MOV P1, R1
-MOV [P0], P1
-; Load Player.oldx
-MOV P0, 292
-MOV P1, [P0]
-; Preserve left operand in register across right-side evaluation
-MOV R2, P1
-MOV R3, 1
-SHL R3, 4
-MOV R0, R2
-SUB R0, R3
-; Free R3 (last use)
-MOV VX, R0
-; Load Player.oldy
-MOV P0, 294
-MOV P1, [P0]
-; Preserve left operand in register across right-side evaluation
-MOV R3, P1
-MOV R4, 1
-SHL R4, 3
-MOV R0, R3
-ADD R0, R4
-; Free R4 (last use)
-MOV VY, R0
-XOR VC, VC
-MOV P0, 300
-MOV P0, [P0]
-TEXT P0
-; Load Player.oldx
-MOV P0, 292
-MOV P1, [P0]
-; Preserve left operand in register across right-side evaluation
-MOV R2, P1
-MOV R3, 1
-SHL R3, 3
-MOV R0, R2
-ADD R0, R3
-; Free R3 (last use)
-MOV VX, R0
-; Load Player.oldy
-MOV P0, 294
-MOV P1, [P0]
-; Preserve left operand in register across right-side evaluation
-MOV R3, P1
-MOV R4, 1
-SHL R4, 3
-MOV R0, R3
-ADD R0, R4
-; Free R4 (last use)
-MOV VY, R0
-XOR VC, VC
-MOV P0, 300
-MOV P0, [P0]
-TEXT P0
-L44:
-JMP L34
-L35:
+JMP L44
+L45:
 HLT
 
-_func_drawplayer_0:
+_func_renderplayer_0:
+; Function: renderplayer
+; Parameters: x, y, color
+; Locals:  (0 bytes)
+ENTER 0
+MOV P0, FP
+ADD P0, 8
+MOV P1, [P0]
+MOV VX, :P1
+MOV P0, FP
+ADD P0, 6
+MOV P1, [P0]
+MOV VY, :P1
+MOV P0, FP
+ADD P0, 4
+MOV P1, [P0]
+MOV VC, :P1
+TEXT STR0
+MOV P0, FP
+ADD P0, 8
+MOV P1, [P0]
+MOV VX, :P1
+MOV P0, FP
+ADD P0, 6
+MOV P1, [P0]
+; Preserve left operand in register across right-side evaluation
+MOV R3, P1
+MOV R4, 1
+SHL R4, 3
+MOV R1, R3
+ADD R1, R4
+; Free R4 (last use)
+MOV VY, R1
+MOV P0, FP
+ADD P0, 4
+MOV P2, [P0]
+MOV VC, :P2
+TEXT STR1
+MOV SP, FP
+POP FP
+RETN 0
+
+_func_drawplayer_1:
 ; Function: drawplayer
 ; Parameters: 
 ; Locals:  (0 bytes)
@@ -249,35 +141,22 @@ MOV P2, [P0]
 CMP P1, P2
 ; Free P1 (last use)
 ; Free P2 (last use)
-JZ L1
+JZ L3
 ; Load Player.oldx
 MOV P0, 292
 MOV P1, [P0]
-MOV VX, P1
+PUSH P1
 ; Load Player.oldy
 MOV P0, 294
 MOV P1, [P0]
-MOV VY, P1
-XOR VC, VC
-TEXT STR2
-; Load Player.oldx
-MOV P0, 292
-MOV P1, [P0]
-MOV VX, P1
-; Load Player.oldy
-MOV P0, 294
-MOV P1, [P0]
-; Preserve left operand in register across right-side evaluation
-MOV R3, P1
-MOV R4, 1
-SHL R4, 3
-MOV R1, R3
-ADD R1, R4
-; Free R4 (last use)
-MOV VY, R1
-XOR VC, VC
-TEXT STR3
-L1:
+PUSH P1
+XOR R3, R3
+MOV P1, R3
+PUSH P1
+; Free R3 (last use)
+CALL _func_renderplayer_0
+ADD SP, 6
+L3:
 ; Load Player.oldy
 MOV P0, 294
 MOV P1, [P0]
@@ -291,63 +170,37 @@ JZ L5
 ; Load Player.oldx
 MOV P0, 292
 MOV P1, [P0]
-MOV VX, P1
+PUSH P1
 ; Load Player.oldy
 MOV P0, 294
 MOV P1, [P0]
-MOV VY, P1
-XOR VC, VC
-TEXT STR2
-; Load Player.oldx
-MOV P0, 292
-MOV P1, [P0]
-MOV VX, P1
-; Load Player.oldy
-MOV P0, 294
-MOV P1, [P0]
-; Preserve left operand in register across right-side evaluation
-MOV R3, P1
-MOV R4, 1
-SHL R4, 3
-MOV R1, R3
-ADD R1, R4
-; Free R4 (last use)
-MOV VY, R1
-XOR VC, VC
-TEXT STR3
+PUSH P1
+XOR R3, R3
+MOV P1, R3
+PUSH P1
+; Free R3 (last use)
+CALL _func_renderplayer_0
+ADD SP, 6
 L5:
 ; Load Player.x
 MOV P0, 288
 MOV P1, [P0]
-MOV VX, P1
+PUSH P1
 ; Load Player.y
 MOV P0, 290
 MOV P1, [P0]
-MOV VY, P1
-MOV VC, 15
-TEXT STR2
-; Load Player.x
-MOV P0, 288
-MOV P1, [P0]
-MOV VX, P1
-; Load Player.y
-MOV P0, 290
-MOV P1, [P0]
-; Preserve left operand in register across right-side evaluation
-MOV R3, P1
-MOV R4, 1
-SHL R4, 3
-MOV R1, R3
-ADD R1, R4
-; Free R4 (last use)
-MOV VY, R1
-MOV VC, 15
-TEXT STR3
+PUSH P1
+MOV R3, 15
+MOV P1, R3
+PUSH P1
+; Free R3 (last use)
+CALL _func_renderplayer_0
+ADD SP, 6
 MOV SP, FP
 POP FP
 RETN 0
 
-_func_attack_1:
+_func_attack_2:
 ; Function: attack
 ; Parameters: 
 ; Locals:  (0 bytes)
@@ -431,7 +284,7 @@ MOV SP, FP
 POP FP
 RETN 0
 
-_func_keycheck_2:
+_func_keycheck_3:
 ; Function: keycheck
 ; Parameters: 
 ; Locals:  (0 bytes)
@@ -449,7 +302,7 @@ CMP P1, P2
 ; Free P1 (last use)
 ; Free P2 (last use)
 JNZ L11
-CALL _func_attack_1
+CALL _func_attack_2
 L11:
 MOV P0, 302
 MOV P1, [P0]
@@ -651,8 +504,165 @@ L27:
 MOV SP, FP
 POP FP
 RETN 0
-STR2: DEFSTR "O"
-STR3: DEFSTR "X"
-STR28: DEFSTR "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-STR31: DEFSTR "X                              X"
-STR32: DEFSTR "--"
+
+_func_callplayer_4:
+; Function: callplayer
+; Parameters: 
+; Locals:  (0 bytes)
+ENTER 0
+; Load Player.x
+MOV P0, 288
+MOV P1, [P0]
+; Store to Player.oldx
+MOV P0, 292
+MOV [P0], P1
+; Load Player.y
+MOV P0, 290
+MOV P1, [P0]
+; Store to Player.oldy
+MOV P0, 294
+MOV [P0], P1
+; Load Player.counter
+MOV P0, 298
+MOV P1, [P0]
+MOV R0, P1
+ADD P1, 1
+; Store to Player.counter
+MOV P0, 298
+MOV [P0], P1
+CALL _func_keycheck_3
+; Load Player.x
+MOV P0, 288
+MOV P1, [P0]
+MOV P2, 1
+SHL P2, 3
+CMP P1, P2
+; Free P1 (last use)
+; Free P2 (last use)
+JGE L29
+MOV R1, 1
+SHL R1, 3
+; Store to Player.x
+MOV P0, 288
+MOV P1, R1
+MOV [P0], P1
+L29:
+; Load Player.x
+MOV P0, 288
+MOV P1, [P0]
+MOV P2, 240
+CMP P1, P2
+; Free P1 (last use)
+; Free P2 (last use)
+JLE L31
+MOV R1, 240
+; Store to Player.x
+MOV P0, 288
+MOV P1, R1
+MOV [P0], P1
+L31:
+; Load Player.y
+MOV P0, 290
+MOV P1, [P0]
+MOV P2, 1
+SHL P2, 3
+CMP P1, P2
+; Free P1 (last use)
+; Free P2 (last use)
+JGE L33
+MOV R1, 1
+SHL R1, 3
+; Store to Player.y
+MOV P0, 290
+MOV P1, R1
+MOV [P0], P1
+L33:
+; Load Player.y
+MOV P0, 290
+MOV P1, [P0]
+MOV P2, 232
+CMP P1, P2
+; Free P1 (last use)
+; Free P2 (last use)
+JLE L35
+MOV R1, 232
+; Store to Player.y
+MOV P0, 290
+MOV P1, R1
+MOV [P0], P1
+L35:
+CALL _func_drawplayer_1
+; Load Player.counter
+MOV P0, 298
+MOV P1, [P0]
+MOV P2, 255
+CMP P1, P2
+; Free P1 (last use)
+; Free P2 (last use)
+JNZ L37
+XOR R1, R1
+; Store to Player.counter
+MOV P0, 298
+MOV P1, R1
+MOV [P0], P1
+; Load Player.oldx
+MOV P0, 292
+MOV P1, [P0]
+; Preserve left operand in register across right-side evaluation
+MOV R2, P1
+MOV R3, 1
+SHL R3, 4
+MOV R0, R2
+SUB R0, R3
+; Free R3 (last use)
+MOV VX, R0
+; Load Player.oldy
+MOV P0, 294
+MOV P1, [P0]
+; Preserve left operand in register across right-side evaluation
+MOV R3, P1
+MOV R4, 1
+SHL R4, 3
+MOV R0, R3
+ADD R0, R4
+; Free R4 (last use)
+MOV VY, R0
+XOR VC, VC
+MOV P0, 300
+MOV P0, [P0]
+TEXT P0
+; Load Player.oldx
+MOV P0, 292
+MOV P1, [P0]
+; Preserve left operand in register across right-side evaluation
+MOV R2, P1
+MOV R3, 1
+SHL R3, 3
+MOV R0, R2
+ADD R0, R3
+; Free R3 (last use)
+MOV VX, R0
+; Load Player.oldy
+MOV P0, 294
+MOV P1, [P0]
+; Preserve left operand in register across right-side evaluation
+MOV R3, P1
+MOV R4, 1
+SHL R4, 3
+MOV R0, R3
+ADD R0, R4
+; Free R4 (last use)
+MOV VY, R0
+XOR VC, VC
+MOV P0, 300
+MOV P0, [P0]
+TEXT P0
+L37:
+MOV SP, FP
+POP FP
+RETN 0
+STR0: DEFSTR "O"
+STR1: DEFSTR "X"
+STR38: DEFSTR "--"
+STR39: DEFSTR "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+STR42: DEFSTR "X                              X"
